5a9666db086db6aab111d7502b5c9369008f463d
maiktreya <miguel.garcia.duch@gmail.com>
2025-07-16 14:44:48 +0200

    last

--- src/main.py
+++ src/main.py
@@ -6,14 +6,13 @@ from nicegui import app, ui
 
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
-@ui.page('/')
+@ui.page("/")
 def index_page() -> None:
-    with theme.frame('Homepage'):
+    with theme.frame("Homepage"):
         home_page.content()
 
 
 # this call shows that you can also move the whole page creation into a separate file
 all_pages.create()
 
-
-ui.run(title='Getting Started with NiceGUI')
+ui.run(title="Getting Started with NiceGUI")
