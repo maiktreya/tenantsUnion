d07dc1c865165da38fc9d2f3716d7c8bc19f7966
il    iciuv <miguel.garcia.duch@gmail.com>
2025-08-12 01:08:38 +0200

    Revert "refractored 0.1"
    This reverts commit aaac61c0a9ae2e95ef37417fe59588557c19ec63.

--- sindicato_app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .schemas import Conflict, ConflictNote
-
-__all__ = ['Conflict', 'ConflictNote']
--- sindicato_app/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .export import export_to_csv, export_to_json
-
-__all__ = ['export_to_csv', 'export_to_json']
