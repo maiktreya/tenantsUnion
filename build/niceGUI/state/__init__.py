5586563c3fc07d5dcdd48aec72935c56d1f46e73
maiktreya <garciaduchm@gmail.com>
2025-09-16 23:02:31 +0200

    Update __init__.py

--- build/niceGUI/state/__init__.py
+++ build/niceGUI/state/__init__.py
@@ -2,11 +2,9 @@
 
 from .base import BaseTableState, ReactiveValue
 from .app_state import GenericViewState
-from .importer_state import ImporterState
 
 __all__ = [
     "BaseTableState",
     "ReactiveValue",
-    "ImporterState",
     "GenericViewState",
 ]
