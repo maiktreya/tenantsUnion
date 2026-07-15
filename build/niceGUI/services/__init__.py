b91431955da8205e0a36eb3e24ffbb91eb628018
maiktreya <garciaduchm@gmail.com>
2026-07-15 21:51:43 +0200

    updated __init__.py for services modules

--- build/niceGUI/services/__init__.py
+++ build/niceGUI/services/__init__.py
@@ -1,4 +1,10 @@
 from .relational_import_service import MultiTableImportService
 from .federated_courier import run_sync_job
+from .geolink_service import lookup_cadastral_data, to_ewkt_point
 
-__all__ = ["MultiTableImportService", "run_sync_job"]
+__all__ = [
+    "MultiTableImportService",
+    "run_sync_job",
+    "lookup_cadastral_data",
+    "to_ewkt_point",
+]
